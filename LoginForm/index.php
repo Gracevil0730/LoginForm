be9081af49fb1a648e04f3137923b940e98973cd
Gracevil0730 <capricho.gracevil@gmail.com>
2026-03-23 18:49:32 +0800

    Add files via upload

---
+++ LoginForm/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Gracevil Jade H. Capricho</h1>
+    <div class="container">
+    <label for="username"><b>Username:</b></label>
+    <input type="text" placeholder="Enter Username" name="" required>
+
+    <label for="psw"><b>Password:</b></label>
+    <input type="password" placeholder="Enter Password" name="" required>
+
+    <button type="submit">Login</button>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+</div>
+
+</body>
+</html>
